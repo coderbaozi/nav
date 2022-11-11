5320b292b85cc3d04638e9177329c5c5406b4aee
baozi <YHbaozi1988@163.com>
2022-11-11 23:11:38 +0800

    ：lipstick: 实现主页搜索组件

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,9 +1,38 @@
 <template>
-	
+	<div class="search-panel">
+		
+		<t-input-adornment>
+			<t-input :align="position" @click="clickPosition" @change="checkInput" @blur="changePosition" size="large" autofocus placeholder="相信美好的事情终会发生">
+				<template #prefix-icon>
+					<span class="iconfont icon-guge"></span>
+				</template>
+			</t-input>
+		</t-input-adornment>
+
+	</div>
+
 </template>
 
-<script>
+
+<script setup>
+	import {ref} from 'vue';
+	const position=ref('center');
+	const POSITION_CENTER='center';
+	const POSITION_LEFT='left';
+	const clickPosition=()=>{
+		position.value='left'
+	}
+	const changePosition = (value)=>{
+		if(!value){
+			position.value='center';
+		}
+		
+	};
+	const checkInput=()=>{
+		position.value='left'
+	}
 </script>
 
 <style>
+
 </style>
