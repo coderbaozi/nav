fda023dc9b8f14c9a280c33977053c88466d5a9a
baozi <YHbaozi1988@163.com>
2022-11-14 10:54:09 +0800

    :necktie: 封装时间常量池

---
+++ src/constant/timeUnit.js
@@ -0,0 +1,11 @@
+/**
+ * description: 时间单位常量池
+ * author: baozi
+ * @createTime: 2022-11-13 16:42:17
+ */
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+export {SECOND,MINUTE,HOUR,DAY};
