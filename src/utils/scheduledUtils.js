b18fb6b2be6608ff5d095592b0de93e14e457894
baozi <YHbaozi1988@163.com>
2022-11-14 10:52:26 +0800

    :necktie: 封装定时器工具

---
+++ src/utils/scheduledUtils.js
@@ -0,0 +1,20 @@
+/**
+ * description: 封装js 定时执行任务 
+ * author: baozi
+ * @createTime: 2022-11-13 16:25:36
+ */
+
+const scheduleUtils = {
+	/**
+	 * description: 设置执行函数定时任务
+	 * author: baozi
+	 * @param func 定时执行的函数
+	 * @param millisec 执行的时间间隔 单位：毫秒
+	 * @return 
+	 */
+	setScheduledFunctionTask: (func,millisec) => {
+		let functionTask = setInterval(func,millisec);
+		return functionTask;
+	}
+}
+export default scheduleUtils;
