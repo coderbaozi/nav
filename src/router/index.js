434e5091868a30c5250fcec9226056528b88b675
baozi <YHbaozi1988@163.com>
2022-12-01 23:02:49 +0800

    :fire: 删除路由冗余代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+	createRouter,
+	createWebHistory
+} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/test',
-      name: '测试功能',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/test.vue')
-    },
-	{
-		path: '/home',
-		name: '起始页',
-		component: ()=> import('../views/home.vue'),
-		children: [
-			{
-				path: 'search',
-				name: '搜索框',
-				component: ()=> import('../components/search.vue')
-			}
-		]
-	}
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [{
+			path: '/test',
+			name: '测试功能',
+			component: () => import('../views/test.vue')
+		},
+		{
+			path: '/home',
+			name: '起始页',
+			component: () => import('../views/home.vue'),
+		}
+	]
 });
 
 export default router;
