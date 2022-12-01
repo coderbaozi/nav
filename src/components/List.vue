f60a2f957b58ea6344bc546c511432435b8eb2d6
baozi <YHbaozi1988@163.com>
2022-12-01 22:49:57 +0800

    :wrench: 添加vite-svg-loader配置

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,17 +4,7 @@
 			<t-list>
 				<t-list-item :class="selectedIndex == index ?'selected':''" @mouseover="selectedItem(item,index)" :key="index" v-for="(item,index) in content">
 					<div :class="['cm-list-panel']">
-						<div :class="['cm-list-icon']">
-							<svg t="1668862845981" class="search-icon" viewBox="0 0 1024 1024" version="1.1"
-								 p-id="2224" width="18" height="18">
-								<path
-									d="M465.454545 884.363636C223.809164 884.363636 27.927273 688.481745 27.927273 446.836364S223.809164 9.309091 465.454545 9.309091 902.981818 205.190982 902.981818 446.836364 707.099927 884.363636 465.454545 884.363636z m0-55.854545c210.795055 0 381.672727-170.877673 381.672728-381.672727S676.2496 65.163636 465.454545 65.163636 83.781818 236.041309 83.781818 446.836364 254.659491 828.509091 465.454545 828.509091z"
-									fill="#0069F6" p-id="2225"></path>
-								<path
-									d="M791.078956 839.883175m19.747564-19.747564l0 0q19.747564-19.747564 39.495128 0l144.815469 144.815469q19.747564 19.747564 0 39.495127l0 0q-19.747564 19.747564-39.495128 0l-144.815469-144.815468q-19.747564-19.747564 0-39.495128Z"
-									fill="#5ED7BC" p-id="2226"></path>
-							</svg>
-						</div>
+						<icon-search :class="['cm-list-icon']"></icon-search>
 						<div :class="['cm-association-content']">
 							{{item}}
 						</div>
@@ -27,6 +17,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import iconSearch from '@/assets/icon/svg/Search.svg'
 	const props = defineProps({
 		content: {
 			type: Array,
@@ -61,17 +52,15 @@ import { ref } from 'vue';
 		justify-content: flex-start;
 	}
 	.t-list-item{
-		width: calc(30vw - 48px);
 		cursor: pointer;
+		width: calc(30vw - 48px);
 	}
 	.cm-list-panel{
 		display: flex;
 		.cm-list-icon {
 			padding-right: 30px;
-			.search-icon {
-				width: 15px;
-				height: 15px;
-			}
+			width: 18px;
+			height: 18px;
 		}
 	}
 	
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),svgLoader()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
