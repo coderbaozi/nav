29a5111c139db7b1a57de8c2636613e848b83f9a
baozi <YHbaozi1988@163.com>
2022-12-06 13:26:05 +0800

    :necktie: 封装请求url常量

---
+++ src/constant/sugConstant.js
@@ -0,0 +1,6 @@
+const GOOGLE_ASSOCAITION_URL =
+  'http://suggestqueries.google.com/complete/search?client=youtube&q=#content#&jsonp=window.google.sug'
+const BING_ASSOCAITION_URL = 'http://api.bing.com/qsonhs.aspx?type=cb&q=#content#&cb=window.bing.sug'
+const BAIDU_ASSOCAITION_URL = 'http://suggestion.baidu.com/su?wd=#content#&cb=window.baidu.sug'
+
+export { GOOGLE_ASSOCAITION_URL, BING_ASSOCAITION_URL, BAIDU_ASSOCAITION_URL }
