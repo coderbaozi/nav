5ecb6b8c788776957ce940f3b1147a389455d092
baozi <YHbaozi1988@163.com>
2022-12-06 13:23:43 +0800

    :necktie: 增加关于页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
-import {
-	createRouter,
-	createWebHistory
-} from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [{
-			path: '/test',
-			name: '测试功能',
-			component: () => import('../views/test.vue')
-		},
-		{
-			path: '/home',
-			name: '起始页',
-			component: () => import('../views/home.vue'),
-		}
-	]
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: '关于我们',
+      component: () => import('../views/about.vue'),
+    },
+    {
+      path: '/home',
+      name: '起始页',
+      component: () => import('../views/home.vue'),
+    },
+    {
+      path: '/test',
+      name: '测试功能',
+      component: () => import('../views/test.vue'),
+    },
+  ],
+})
 
-export default router;
+export default router
---
+++ src/views/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>关于我们</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang='less'>
+
+</style>
